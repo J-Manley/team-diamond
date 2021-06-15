53d1f73d805ceb410f630317b7f641ef218c1622
Joel <j_90@live.co.uk>
2021-06-15 04:53:29 +0100

    adds hero content

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+
+        grey: colors.gray,
+        trueGrey: colors.trueGray,
+        warmGrey: colors.warmGray,
+        red: colors.red,
+        green: colors.green,
+        emerald: colors.emerald,
+        teal: colors.teal,
+        cyan: colors.cyan,
+        lightBlue: colors.lightBlue,
+        blue: colors.lightBlue,
+        indigo: colors.indigo,
+        violet: colors.violet,
+        purple: colors.purple,
+        fuchsia: colors.fuchsia,
+        pink: colors.pink,
+        rose: colors.rose,
+      },
+
+      fontFamily: { damion: ["'Damion'", "cursive"] },
+      fontFamily: { rubik: ["'Rubik'", "helvetica", "sans-serif"] },
+    },
+  },
+  variants: {
+    extend: {
+      borderWidth: ["hover"],
+      borderColor: ["hover"],
+    },
+  },
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio")],
+};
