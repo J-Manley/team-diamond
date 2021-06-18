790e8a2e0c9dd727befa3bebf55abdf389b0c245
Joel <j_90@live.co.uk>
2021-06-18 18:22:13 +0100

    alters vite config file to include all pages in input

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,10 @@ module.exports = {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
+        classes: resolve(__dirname, "classes.html"),
+        instructors: resolve(__dirname, "instructors.html"),
         about: resolve(__dirname, "about.html"),
-        contact: resolve(__dirname, "about.html"),
+        contact: resolve(__dirname, "contact.html"),
       },
     },
   },
